ad2d90eea94b91d87254db0636faf1a1b1089008
Anton Olguin <antonolguin1@gmail.com>
2022-03-19 20:46:00 -0600

    initial commit

--- Source/FPSGame/Public/FPSObjectiveActor.h
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -19,8 +19,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UStaticMeshComponent
-
+	// UStaticMeshComponent
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
